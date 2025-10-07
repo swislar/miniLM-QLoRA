f7017dcf10e7eb8fc7c2f82ae7bc56750c3e164d
happlesful <Swislar1108@gmail.com>
2025-10-07 14:12:07 +0800

    Add: Start and End token for training pairs

--- Datasets/Dataset.py
+++ Datasets/Dataset.py
@@ -5,5 +5,5 @@ def mental_health_counseling():
     # https://huggingface.co/datasets/Amod/mental_health_counseling_conversations
     # Special Tokens for QWEN - https://qwen.readthedocs.io/en/latest/getting_started/concepts.html
     ds = load_dataset("Amod/mental_health_counseling_conversations")['train']
-    return ds.map(lambda x: {'Pair': f"<|im_start|>USER:\n{x['Context']}<|im_end|>\n<|im_start|>ASSISTANT:\n{x['Response']}<|im_end|><|endoftext|>"})
+    return ds.map(lambda x: {'text': f"<|im_start|>USER:\n{x['Context']}<|im_end|>\n<|im_start|>ASSISTANT:\n{x['Response']}<|im_end|><|endoftext|>"})
     
