a84e4de57e5485f8e58dfc4bc6c4105edb142dd1
happlesful <Swislar1108@gmail.com>
2025-10-07 14:13:14 +0800

    Add: Training script

---
+++ main.py
@@ -0,0 +1,11 @@
+from Models.Qwen1_8 import Qwen1_8
+from SFT.LoRA import LoRA
+from Datasets.Dataset import mental_health_counseling
+
+def run():
+    qwen = Qwen1_8()
+    ds = mental_health_counseling()
+    LoRA().train(qwen, ds)
+    
+if __name__ == "__main__":
+    run()
