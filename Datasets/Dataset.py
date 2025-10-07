5fb56c4ca59aa6f3dd777ece97df5e32492d3268
happlesful <Swislar1108@gmail.com>
2025-10-07 10:25:43 +0800

    Add: Pair preprocessing for SFT

--- Datasets/Dataset.py
+++ Datasets/Dataset.py
@@ -1,10 +1,9 @@
 from datasets import load_dataset
 
-class Dataset:
-    def __init__(self):
-        return
-    
-    def mental_health_counseling(self):
-        # https://huggingface.co/datasets/Amod/mental_health_counseling_conversations
-        return load_dataset("Amod/mental_health_counseling_conversations")['train']
+
+def mental_health_counseling():
+    # https://huggingface.co/datasets/Amod/mental_health_counseling_conversations
+    # Special Tokens for QWEN - https://qwen.readthedocs.io/en/latest/getting_started/concepts.html
+    ds = load_dataset("Amod/mental_health_counseling_conversations")['train']
+    return ds.map(lambda x: {'Pair': f"<|im_start|>USER:\n{x['Context']}<|im_end|>\n<|im_start|>ASSISTANT:\n{x['Response']}<|im_end|><|endoftext|>"})
     
